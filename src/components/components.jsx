aa6b03c0f2e209fbe9a7d4ee3340b2d212bbfb4d
Geekki1245 <56454778+Geekki1245@users.noreply.github.com>
2025-02-23 14:03:25 -0300

    feat: adding Main and SectionTitle components.

--- src/components/components.jsx
+++ src/components/components.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 
 export default {
     "shareOnSocialMedia": ShareOnSocialMedia ,
@@ -5,6 +6,8 @@ export default {
     "logo": Logo ,
     "menuAndMediaShare": MenuAndMediaShare,
     "header": Header,
+    "main": Main,
+    "sectionTitle": SectionTitle,
 }
 
 
@@ -89,5 +92,43 @@ export function IconBtn({ id=Symbol().toString(), iconSrc, iconAlt, iconSize="1.
       <img src={iconSrc} className={style}
         alt={iconAlt}/>
     </button>
+  );
+}
+
+export function Main()
+{
+  return (
+    <div>
+      <SectionTitle title="Sobre"/>
+    </div>
+  );
+}
+
+export function SectionTitle({ title })
+{
+  const standardRootFontSize = 16; // 16px
+
+  const h2Ref = React.useRef(null);
+  const hrRef = React.useRef(null);
+  const [hrWidth, setHrWidth] = React.useState(300/standardRootFontSize); // 18.75rem
+
+  React.useEffect(() => {
+    if (h2Ref.current && h2Ref.current.offsetWidth >= 
+      ((hrWidth * standardRootFontSize) - 50)
+    ) {
+      let h2Width = h2Ref.current.offsetWidth;
+      setHrWidth((h2Width + 100)/standardRootFontSize);
+      hrRef.current.style.width = (hrWidth).toString() + "rem";
+    }
+  }, []); // Will load once after the first render
+
+  return (
+    <div id={`setionTitle${title}Container`} 
+      className="w-fit flex flex-col gap-[10px] items-center">
+      <h2 id={`sectionTitle${title}Title`} 
+        ref={h2Ref} className="font-medium text-[1.5rem]">{title}</h2>
+      <hr id={`sectionTitle${title}Line`} 
+        ref={hrRef} style={{width: (hrWidth).toString() + "rem"}}/>
+    </div>
   );
 }
