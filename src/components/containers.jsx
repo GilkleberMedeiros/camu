39760ef08ad64bd048ada2a5d8f21ba6d60b17ed
Geekki1245 <56454778+Geekki1245@users.noreply.github.com>
2025-03-19 13:23:00 -0300

    fix: fixing page break at 550px or lower widths.

--- src/components/containers.jsx
+++ src/components/containers.jsx
@@ -22,7 +22,7 @@ export function ZebraOpeningHoursTable({ openingHoursInfo })
       <tbody>
         <tr 
           id="tableRowSegunda" 
-          className="flex text-[0.75rem] font-medium bg-secondary-black text-white p-[7px]"
+          className="flex text-[0.75rem] font-medium bg-secondary-black text-white p-[7px] max-c-s:p-[6px]"
         >
           <td className="inline-flex justify-center w-[50%]">Segunda-feira</td>
           <td className="inline-flex justify-center w-[50%]" >
@@ -31,7 +31,7 @@ export function ZebraOpeningHoursTable({ openingHoursInfo })
         </tr>
         <tr 
           id="tableRowTerca" 
-          className="flex text-[0.75rem] font-medium bg-secondary-white text-black p-[7px]"
+          className="flex text-[0.75rem] font-medium bg-secondary-white text-black p-[7px] max-c-s:p-[6px]"
         >
           <td className="inline-flex justify-center w-[50%]" >Terça-feira</td>
           <td className="inline-flex justify-center w-[50%]" >
@@ -40,7 +40,7 @@ export function ZebraOpeningHoursTable({ openingHoursInfo })
         </tr>
         <tr 
           id="tableRowQuarta" 
-          className="flex text-[0.75rem] font-medium bg-secondary-black text-white p-[7px]"
+          className="flex text-[0.75rem] font-medium bg-secondary-black text-white p-[7px] max-c-s:p-[6px]"
         >
           <td className="inline-flex justify-center w-[50%]" >Quarta-feira</td>
           <td className="inline-flex justify-center w-[50%]" >
@@ -49,7 +49,7 @@ export function ZebraOpeningHoursTable({ openingHoursInfo })
         </tr>
         <tr 
           id="tableRowQuinta" 
-          className="flex text-[0.75rem] font-medium bg-secondary-white text-black p-[7px]"
+          className="flex text-[0.75rem] font-medium bg-secondary-white text-black p-[7px] max-c-s:p-[6px]"
         >
           <td className="inline-flex justify-center w-[50%]" >Quinta-feira</td>
           <td className="inline-flex justify-center w-[50%]" >
@@ -58,7 +58,7 @@ export function ZebraOpeningHoursTable({ openingHoursInfo })
         </tr>
         <tr 
           id="tableRowSexta" 
-          className="flex text-[0.75rem] font-medium bg-secondary-black text-white p-[7px]"
+          className="flex text-[0.75rem] font-medium bg-secondary-black text-white p-[7px] max-c-s:p-[6px]"
         >
           <td className="inline-flex justify-center w-[50%]" >Sexta-feira</td>
           <td className="inline-flex justify-center w-[50%]" >
@@ -67,7 +67,7 @@ export function ZebraOpeningHoursTable({ openingHoursInfo })
         </tr>
         <tr 
           id="tableRowSabado" 
-          className="flex text-[0.75rem] font-medium bg-secondary-white text-black p-[7px]"
+          className="flex text-[0.75rem] font-medium bg-secondary-white text-black p-[7px] max-c-s:p-[6px]"
         >
           <td className="inline-flex justify-center w-[50%]" >Sabádo</td>
           <td className="inline-flex justify-center w-[50%]" >
@@ -76,7 +76,7 @@ export function ZebraOpeningHoursTable({ openingHoursInfo })
         </tr>
         <tr 
           id="tableRowDomingo" 
-          className="flex text-[0.75rem] font-medium bg-secondary-black text-white p-[7px]"
+          className="flex text-[0.75rem] font-medium bg-secondary-black text-white p-[7px] max-c-s:p-[6px]"
         >
           <td className="inline-flex justify-center w-[50%]" >Domingo</td>
           <td className="inline-flex justify-center w-[50%]" >
