754da0cc89de60f28d94c87707bed0a5bd6da3a5
Geekki1245 <56454778+Geekki1245@users.noreply.github.com>
2025-03-21 14:30:07 -0300

    fix: ChevronRightIcon tag properties name.

--- src/components/icons.jsx
+++ src/components/icons.jsx
@@ -62,10 +62,10 @@ export function ChevronRightIcon({ style, children, ...rest })
       viewBox="0 0 24 24" 
       fill="none" 
       stroke="#09090B" 
-      stroke-width="2" 
-      stroke-linecap="round" 
-      stroke-linejoin="round" 
-      class={twMerge("lucide lucide-chevron-right", style)}
+      strokeWidth="2" 
+      strokeLinecap="round" 
+      strokeLinejoin="round" 
+      className={twMerge("lucide lucide-chevron-right", style)}
       {...rest}
     >
       <path d="m9 18 6-6-6-6"/>
