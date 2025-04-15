895e125c247035d4b37626a74a4ca5b7b18ca870
Geekki1245 <56454778+Geekki1245@users.noreply.github.com>
2025-04-15 12:06:44 -0300

    fix: locations page content not fitting the viewport height.

--- src/pages/locations.jsx
+++ src/pages/locations.jsx
@@ -4,11 +4,13 @@ import { Header, Locations as Locs, Footer } from "../components/main";
 export function Locations()  
 {
     return (
-        <>
-            <Header />
-            <Locs />
+        <div className="min-h-[100%] flex flex-col justify-between">
+            <div>
+                <Header />
+                <Locs />
+            </div>
             <Footer />
-        </>
+        </div>
     );
 }
 
