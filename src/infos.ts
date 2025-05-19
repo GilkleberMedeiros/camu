fa2a49a94d80b2bc44b0d2ae143d196367e86ed5
Geekki1245 <56454778+Geekki1245@users.noreply.github.com>
2025-05-19 15:20:41 -0300

    style: formatting infos.ts better.

--- src/infos.ts
+++ src/infos.ts
@@ -4,17 +4,17 @@
 import infos from "@/infos.json";
 
 
-let siteName = infos.name;
-let shareLinks = infos.infos.share.socialMedia;
-
 // serving correct domains
 const env = infos.infos.domains.env;
 let domains = infos.infos.domains;
 infos.infos.domains.original = (domains as Record<string, any>)[env].original;
 infos.infos.domains.friendly = (domains as Record<string, any>)[env].friendly;
 
-const domainOrigin = infos.infos.domains.friendly;
 // Appending query params to whasapp share urls
+const domainOrigin = infos.infos.domains.friendly;
+let siteName = infos.name;
+let shareLinks = infos.infos.share.socialMedia;
+
 let whatsLinkName = "whatsapp";
 let whatsUrls = shareLinks.filter((value) => value.name.toLowerCase() === whatsLinkName);
 let whatsUrl = new URL(whatsUrls[0].link);
